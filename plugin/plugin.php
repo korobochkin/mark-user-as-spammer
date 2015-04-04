735b65feeffb772bfb7cd3f43a48750a288e4d1b
Kolya Korobochkin <me@korobochkin.com>
2015-04-04 12:44:06 +0300

    Explain that we do on uninstall process

--- plugin/plugin.php
+++ plugin/plugin.php
@@ -234,13 +234,16 @@ class Mark_User_As_Spammer {
 	}
 
 	public static function on_uninstall() {
+		// The uninstall plugin must be this file
+		// The current user can activate plugins
 		if( __FILE__ != WP_UNINSTALL_PLUGIN || ! current_user_can( 'activate_plugins') ){
 			return;
 		}
 
+		// Additional check
 		check_admin_referer( 'bulk-plugins' );
 
-		// Delete
+		// Delete user metas with `mark_user_as_spammer` meta_value
 		global $wpdb;
 		$wpdb->delete( $wpdb->usermeta, array( 'meta_key' => 'mark_user_as_spammer' ), array ( '%s' ) );
 	}
