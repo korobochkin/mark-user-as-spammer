521f39ff1da2e2ce12b188779f9ea73af96a3fc5
Kolya Korobochkin <me@korobochkin.com>
2015-11-02 22:13:32 +0300

    Load Users Page

--- plugin/source/Plugin.php
+++ plugin/source/Plugin.php
@@ -11,7 +11,7 @@ class Plugin {
 
 		if ( is_admin() ) {
 			add_filter( 'user_row_actions', array( 'Korobochkin\MarkUserAsSpammer\Admin\UserRowActions', 'render' ), 10, 2);
-			add_action( 'load-users.php', array( __CLASS__, 'load_users_page' ) );
+			add_action( 'load-users.php', array( 'Korobochkin\MarkUserAsSpammer\Admin\LoadUsersPage', 'load_users_page' ) );
 			add_action( 'admin_notices',  array( __CLASS__, 'admin_notices' ) );
 		}
 	}
