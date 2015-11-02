2503906a1655e0e8232b42e62902dbcef0cf228e
Kolya Korobochkin <me@korobochkin.com>
2015-11-02 22:19:44 +0300

    function name

--- plugin/source/Plugin.php
+++ plugin/source/Plugin.php
@@ -12,7 +12,7 @@ class Plugin {
 		if ( is_admin() ) {
 			add_filter( 'user_row_actions', array( 'Korobochkin\MarkUserAsSpammer\Admin\UserRowActions', 'render' ), 10, 2);
 			add_action( 'load-users.php', array( 'Korobochkin\MarkUserAsSpammer\Admin\LoadUsersPage', 'load_users_page' ) );
-			add_action( 'admin_notices',  array( 'Korobochkin\MarkUserAsSpammer\Admin\AdminNotices', 'admin_notices' ) );
+			add_action( 'admin_notices',  array( 'Korobochkin\MarkUserAsSpammer\Admin\AdminNotices', 'add_notices' ) );
 		}
 	}
 }
