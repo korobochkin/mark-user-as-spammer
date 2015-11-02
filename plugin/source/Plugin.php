86a9cbc30908f0376809c289fc0192dcf3decc85
Kolya Korobochkin <me@korobochkin.com>
2015-11-02 22:06:05 +0300

    Auth

--- plugin/source/Plugin.php
+++ plugin/source/Plugin.php
@@ -7,5 +7,7 @@ class Plugin {
 
 	public static function run() {
 		add_action( 'plugins_loaded', array( 'Korobochkin\MarkUserAsSpammer\Translations', 'load_translations' ) );
+
+		add_filter( 'authenticate', array( 'Korobochkin\MarkUserAsSpammer\Authenticate\Authenticate', 'authenticate' ), 99 );
 	}
 }
