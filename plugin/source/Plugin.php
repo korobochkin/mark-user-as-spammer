45756302def47a9930b8d4dac7873d1ea4034a97
Kolya Korobochkin <me@korobochkin.com>
2015-11-02 22:22:24 +0300

    typo

--- plugin/source/Plugin.php
+++ plugin/source/Plugin.php
@@ -11,7 +11,7 @@ class Plugin {
 
 		if ( is_admin() ) {
 			add_filter( 'user_row_actions', array( 'Korobochkin\MarkUserAsSpammer\Admin\UserRowActions', 'render' ), 10, 2);
-			add_action( 'load-users.php', array( 'Korobochkin\MarkUserAsSpammer\Admin\LoadUsersPage', 'load_users_page' ) );
+			add_action( 'load-users.php', array( 'Korobochkin\MarkUserAsSpammer\Admin\LoadUsersPage', 'render' ) );
 			add_action( 'admin_notices',  array( 'Korobochkin\MarkUserAsSpammer\Admin\AdminNotices', 'add_notices' ) );
 		}
 	}
