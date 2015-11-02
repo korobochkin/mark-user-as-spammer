45ca6555146c08ff0cc399ba8f3dd6015e5add79
Kolya Korobochkin <me@korobochkin.com>
2015-11-02 22:09:12 +0300

    User Row Actions

--- plugin/source/Plugin.php
+++ plugin/source/Plugin.php
@@ -7,7 +7,12 @@ class Plugin {
 
 	public static function run() {
 		add_action( 'plugins_loaded', array( 'Korobochkin\MarkUserAsSpammer\Translations', 'load_translations' ) );
-
 		add_filter( 'authenticate', array( 'Korobochkin\MarkUserAsSpammer\Authenticate\Authenticate', 'authenticate' ), 99 );
+
+		if ( is_admin() ) {
+			add_filter( 'user_row_actions', array( 'Korobochkin\MarkUserAsSpammer\Admin\UserRowActions', 'render' ), 10, 2);
+			add_action( 'load-users.php', array( __CLASS__, 'load_users_page' ) );
+			add_action( 'admin_notices',  array( __CLASS__, 'admin_notices' ) );
+		}
 	}
 }
